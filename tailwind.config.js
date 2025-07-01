957ba14a2f64b169bb5e39a6e4d918f49ba8deb7
fadi-zaghdoud <fadi.zaghdoud@esprit.tn>
2025-07-01 23:54:42 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      lexend: ['Lexend', 'sans-serif'],
+    }, 
+  },
+  plugins: [],
+}
